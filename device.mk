024b68b2cf7d47ca633109ef297a2b70b4bc2ca5
Tom Rosier <tom.rosier92@gmail.com>
2019-11-19 17:01:19 +0000

    Removed some un needed stuff

--- device.mk
+++ device.mk
@@ -33,14 +33,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
-# Biometrics
-# PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.tbx304
-
-# Permissions
-# PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
